1bf946400c3ae9d1349e7227e77010c31190715f
plphuc <phamluumyphuc@gmail.com>
2023-08-30 20:18:29 +0700

    [add] MainSection component to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { useState } from 'react';
+
+import MainSection from 'components/MainSection/MainSection';
+// import DetailsSection from 'components/DetailsSection/DetailsSection';
 import SearchSection from 'components/SearchSection/SearchSection';
 
 import styles from './App.module.css';
 
 function App() {
+  const [weatherForecast, setWeatherForecast] = useState({});
+
+  function setWeatherData(weatherForecastObj) {
+    setWeatherForecast(weatherForecastObj);
+  }
+
   return (
     <div className={styles.wrapper}>
-      <SearchSection/>
+      <div className={styles.mainSectionWrapper}>
+        <SearchSection onChangeWeatherData={setWeatherData} />
+
+        {Object.keys(weatherForecast).length !== 0 && (
+          <MainSection data={weatherForecast.currentWeather} />
+        )}
+      </div>
+
+      {/* {Object.keys(weatherForecast).length !== 0 && (
+        <div className={styles.detailsSectionWrapper}>
+          <DetailsSection data={weatherForecast} />
+        </div>
+      )} */}
     </div>
   );
 }
