01d8bd1c7f5c1c5a09bc1a0aa599d379a709d5f0
plphuc <phamluumyphuc@gmail.com>
2023-08-31 14:05:24 +0700

    [add] useContext

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import { useState } from 'react';
+import { createContext, useState } from 'react';
 
 import MainSection from 'components/MainSection/MainSection';
 // import DetailsSection from 'components/DetailsSection/DetailsSection';
 import SearchSection from 'components/SearchSection/SearchSection';
 
 import styles from './App.module.css';
+export const weatherDataContext = createContext();
 
 function App() {
   const [weatherForecast, setWeatherForecast] = useState({});
 
-  function setWeatherData(weatherForecastObj) {
-    setWeatherForecast(weatherForecastObj);
-  }
-
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.mainSectionWrapper}>
-        <SearchSection onChangeWeatherData={setWeatherData} />
+    <weatherDataContext.Provider value={weatherForecast}>
+      <div className={styles.wrapper}>
+        <div className={styles.mainSectionWrapper}>
+          <SearchSection onChangeWeatherData={setWeatherForecast} />
 
-        {Object.keys(weatherForecast).length !== 0 && (
-          <MainSection data={weatherForecast.currentWeather} />
-        )}
-      </div>
-
-      {/* {Object.keys(weatherForecast).length !== 0 && (
-        <div className={styles.detailsSectionWrapper}>
-          <DetailsSection data={weatherForecast} />
+          {Object.keys(weatherForecast).length !== 0 && (
+            <MainSection />
+          )}
         </div>
-      )} */}
-    </div>
+
+        {/* {Object.keys(weatherForecast).length !== 0 && (
+          <div className={styles.detailsSectionWrapper}>
+            <DetailsSection data={weatherForecast} />
+          </div>
+        )} */}
+      </div>
+    </weatherDataContext.Provider>
   );
 }
 
