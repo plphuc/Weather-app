f73c767eb4c09f08ad56bf01fc12e4b9a55e94ae
plphuc <phamluumyphuc@gmail.com>
2023-09-03 14:44:56 +0700

    [change] improve readability of MainSection

--- src/components/MainSection/MainSection.js
+++ src/components/MainSection/MainSection.js
@@ -3,34 +3,36 @@ import { MdLocationOn } from 'react-icons/md';
 
 import { useWeatherInfo } from 'WeatherInfoProvider';
 import WeatherIcon from 'components/WeatherIcon/WeatherIcon';
-import * as utilsFunc from 'utils/convertFunc'
+import * as utilsFunc from 'utils/convertFunc';
 
 import styles from './MainSection.module.css';
 
 function MainSection(props) {
-  const weather = useWeatherInfo()
+  const weatherInfo = useWeatherInfo();
+  const { main, weather } = weatherInfo.currentWeatherForecast || '';
+  const dateString =
+    weatherInfo.currentWeatherForecast &&
+    utilsFunc.formatEpochToDateString(weatherInfo.currentWeatherForecast.dt);
+  const temperature =
+    main && Math.round(utilsFunc.convertKevinToCelcius(main.temp));
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.weatherIconWrapper}>
-        <WeatherIcon iconName={weather.currentWeatherForecast.weather[0].icon} />
-      </div>
+    weatherInfo.currentWeatherForecast && (
+      <div className={styles.wrapper}>
+        <div className={styles.weatherIconWrapper}>
+          <WeatherIcon iconName={weather[0].icon} />
+        </div>
 
-      <div className={styles.weatherInfoWrapper}>
-        <span className={styles.temperatureInfo}>
-          {utilsFunc.convertKevinToCelcius(weather.currentWeatherForecast.main.temp)}°C
-        </span>
-        <span className={styles.weatherCondition}>
-          {weather.currentWeatherForecast.weather[0].main}
-        </span>
-        <span className={styles.dateInfo}>
-          Today • {utilsFunc.convertEpochToDate(weather.currentWeatherForecast.dt)}
-        </span>
-        <span className={styles.locationInfo}>
-          <MdLocationOn />
-          {weather.currentWeatherForecast.name}
-        </span>
+        <div className={styles.weatherInfoWrapper}>
+          <span className={styles.temperatureInfo}>{temperature}°C</span>
+          <span className={styles.weatherCondition}>{weather[0].main}</span>
+          <span className={styles.dateInfo}>Today • {dateString}</span>
+          <span className={styles.locationInfo}>
+            <MdLocationOn />
+            {weatherInfo.currentWeatherForecast.name}
+          </span>
+        </div>
       </div>
-    </div>
+    )
   );
 }
 
