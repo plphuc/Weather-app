51fb96b111dce7053ab9611968f1f2101c910e5c
plphuc <phamluumyphuc@gmail.com>
2023-09-05 00:30:01 +0700

    [remove] unnecessary function

--- src/utils/convertFunc.js
+++ src/utils/convertFunc.js
@@ -2,18 +2,13 @@ export function convertKevinToCelcius(temperature) {
   return temperature - 273.15;
 }
 
-export function convertEpochToDateObj(epochTime) {
-  const date = new Date(epochTime * 1000);
-  return date;
-}
-
-export function formatEpochToDateString(epochTime) {
-  const convertedDate = convertEpochToDateObj(epochTime);
+export function formatDateString(dateString) {
+  const dateObj = new Date(dateString);
   const options = {
     weekday: 'short',
     day: '2-digit',
     month: 'short',
   };
-  const formattedDate = convertedDate.toLocaleDateString(undefined, options)
+  const formattedDate = dateObj.toLocaleDateString(undefined, options)
   return formattedDate;
 }
