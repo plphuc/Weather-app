953e6478f999d3c2974893160d0a48019af3668e
plphuc <phamluumyphuc@gmail.com>
2023-09-12 14:41:01 +0700

    [change] split return into new component

--- src/components/Highlight/Highlight.js
+++ src/components/Highlight/Highlight.js
@@ -1,48 +1,60 @@
 import React from 'react';
 import { MdAssistantNavigation } from 'react-icons/md';
 
-import * as utilsFunc from 'utils/convertFunc';
 import { useWeatherInfo } from 'WeatherInfoProvider';
-import ProgressBar from 'components/ProgressBar/ProgressBar';
+import * as utilsFunc from 'utils/convertFunc';
 import HighlightCard from 'components/HighlightCard/HighlightCard';
-
+import ProgressBar from 'components/ProgressBar/ProgressBar';
 import styles from './Highlight.module.css';
 
+function WindStatus(props) {
+  const { windSpeed, windDirection } = props;
+  return (
+    <HighlightCard title="Wind Status">
+      <span className={styles.valueInfo}>{windSpeed}mps</span>
+      <div className={styles.windDirectionWrapper}>
+        <div className={styles.windDirectionIcon}>
+          <MdAssistantNavigation />
+        </div>
+        <span>{windDirection}</span>
+      </div>
+    </HighlightCard>
+  );
+}
+
+function HumidityStatus(props) {
+  const { humidity } = props;
+  return (
+    <HighlightCard title="Humidity">
+      <span className={styles.valueInfo}>{humidity}%</span>
+      <div className={styles.humidBarWrapper}>
+        <ProgressBar humidityPercentage={humidity} progressBarClass={styles.humidBarContainer} />
+      </div>
+    </HighlightCard>
+  );
+}
+
+function SpecifiedHighlightCard(props) {
+  const { title, value } = props;
+  return (
+    <HighlightCard title={title}>
+      <span className={styles.valueInfo}>{value}</span>
+    </HighlightCard>
+  );
+}
 function Highlight(props) {
   const { current } = useWeatherInfo();
   const windDirection = utilsFunc.convertDegreesToCompass(current.windInfo.deg);
   const visibilityInKm = (current.visibility / 1000).toFixed(1);
   return (
     <div className={styles.todayHighLightWrapper}>
-      <span className={styles.todayHigLightTitle}>Today's Highlights</span>
-      
+      <span className={styles.todayHighLightTitle}>Today's Highlights</span>
+
       <div className={styles.todayHighLightContainer}>
-        <HighlightCard title="Wind Status">
-          <span className={styles.valueInfo}>{current.windInfo.speed}mps</span>
-          <div className={styles.windDirectionWrapper}>
-            <div className={styles.windDirectionIcon}>
-              <MdAssistantNavigation />
-            </div>
-            <span>{windDirection}</span>
-          </div>
-        </HighlightCard>
-
-        <HighlightCard title="Humidity">
-          <span className={styles.valueInfo}>{current.humidity}%</span>
-          <div className={styles.humidBarWrapper}>
-            <div className={styles.humidBarContainer}>
-              <ProgressBar humidityPct = {current.humidity} progressBarHeight="8px" />
-            </div>
-          </div>
-        </HighlightCard>
-
-        <HighlightCard title="Visibility">
-          <span className={styles.valueInfo}>{visibilityInKm} km</span>
-        </HighlightCard>
-        
-        <HighlightCard title="Air Pressure">
-          <span className={styles.valueInfo}>{current.airPressure} mb</span>
-        </HighlightCard>
+        <WindStatus windSpeed={current.windInfo.speed} windDirection={windDirection} />
+        <HumidityStatus humidity={current.humidity} />
+        <SpecifiedHighlightCard title="Visibility" value={`${visibilityInKm} km`} />
+        <SpecifiedHighlightCard title="Air Pressure" value={`${current.airPressure} mb`} />
       </div>
     </div>
   );
