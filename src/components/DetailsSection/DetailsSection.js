431b4469e8c8f556ffa1a345092963632243904d
plphuc <phamluumyphuc@gmail.com>
2023-09-12 14:39:26 +0700

    [change] to early return pattern

--- src/components/DetailsSection/DetailsSection.js
+++ src/components/DetailsSection/DetailsSection.js
@@ -5,19 +5,21 @@ import WeatherCard from 'components/WeatherCard/WeatherCard';
 import Highlight from 'components/Highlight/Highlight';
 
 import styles from './DetailsSection.module.css';
+
 function DetailsSection(props) {
   const { current, nextDays } = useWeatherInfo();
+  if (!current || !nextDays) {
+    return;
+  }
   return (
-    <>
-      {current && nextDays && <div className={styles.detailsSectionWrapper}>
-        <div className={styles.nextDaysForecastWrapper}>
-          {nextDays.map((weatherForecastInDay) => {
-            return <WeatherCard weatherForecast = {weatherForecastInDay}/>
-          })}
-        </div>
-        <Highlight />
-      </div>}
-    </>
+    <div className={styles.detailsSectionWrapper}>
+      <div className={styles.nextDaysForecastWrapper}>
+        {nextDays.map((weatherForecastInDay) => (
+          <WeatherCard weatherForecast={weatherForecastInDay} />
+        ))}
+      </div>
+      <Highlight />
+    </div>
   );
 }
 
