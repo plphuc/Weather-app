7de2b276faeb75387eed393cabca082e3e1517e8
plmphuc <plm.phuc@tego.global>
2024-06-16 17:19:58 +0700

    add transition

--- src/components/HighlightCard/HighlightCard.js
+++ src/components/HighlightCard/HighlightCard.js
@@ -5,8 +5,10 @@ function HighlightCard(props) {
   const { title, children } = props;
   return (
     <div className={classNames(styles.wrapper, "card")}>
-      <span className={styles.title}>{title}</span>
-      <div className={styles.childrenWrapper}>{children}</div>
+      <div className="flex-col flex justify-center items-center hover:scale-105 duration-300 w-full h-full">
+        <span className={styles.title}>{title}</span>
+        <div className={styles.childrenWrapper}>{children}</div>
+      </div>
     </div>
   );
 }
